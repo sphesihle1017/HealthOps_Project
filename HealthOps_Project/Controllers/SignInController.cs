6af2e7c923b698a713aae7c1197e4f9b0847f7eb
Sphesihle Shezi <Sphesihlesbani@gmail.com>
2025-08-29 09:15:02 +0200

    Updated Footer

---
+++ HealthOps_Project/Controllers/SignInController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HealthOps_Project.Controllers
+{
+    public class SignInController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
